8ba7a496e6525c78bb4f906a62817132c1985ef9
Maciek <maciej.pazdej@gmail.com>
2024-03-24 22:30:49 +0100

    test

--- Projekt1/headline.h
+++ Projekt1/headline.h
@@ -1,2 +1,8 @@
 #include <stdio.h>
 
+class Czas {
+public:
+	int sekunda;
+	int minuta;
+	int godzina;
+};
--- Projekt1/main.cpp
+++ Projekt1/main.cpp
@@ -1,8 +1,6 @@
 #include "headline.h"
-class Czas {
 
-};
 int main() {
-	std::cout << "Witam" << '\n';
-	return 0;
+    Czas wartosc;
+    return 0;
 }
